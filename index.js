97a553588df0bad6f8bc1f71cca895df5ead7b40
NishithPandey <pnishith215@gmail.com>
2025-08-13 15:57:08 +0530

    task creation setup

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
+
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -9,7 +11,20 @@ app.use(express.static(path.join(__dirname,'public')));
 
 
 app.get("/", (req,res)=>{
-    res.render("index");
+    fs.readdir(`./files`, (err , files) =>{
+         res.render("index" , {files : files});
+
+    })
+   
+})
+app.post("/create", (req,res)=>{
+    
+
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, (err) => {
+        res.redirect("/")
+
+    })
+   
 })
 
 app.listen(3000);
